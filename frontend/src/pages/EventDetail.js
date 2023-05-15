9c0565ab73c29a7b497b64276842220905224ac9
Ivan Bieliaiev <belyaeve2@icloud.com>
2023-05-15 22:22:57 +0200

    Combine defer with conrol

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,21 +1,32 @@
-import {json, redirect, useRouteLoaderData} from "react-router-dom";
+import { Suspense } from "react";
+
+import { Await, defer, json, redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 const EventDetailPage = (props) => {
-    const data = useRouteLoaderData('event-detail');
-    const event = data.event;
+    const {event, events} = useRouteLoaderData('event-detail');
 
     return (
-        <EventItem event={event} />
+        <>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Await resolve={event}>
+                    {(fetchedEvent) => <EventItem event={fetchedEvent} />}
+                </Await>
+            </Suspense>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Await resolve={events}>
+                    {(fetchedEvents) => <EventsList events={fetchedEvents} />}
+                </Await>
+            </Suspense>
+        </>
     )
 }
 
 export default EventDetailPage;
 
-export const loader = async ({request, params}) => {
-    const { eventId: id } = params;
-
+const loadEvent = async (id) => {
     const response = await fetch(`http://localhost:8080/events/${id}`);
 
     if (response.ok === false) {
@@ -23,8 +34,35 @@ export const loader = async ({request, params}) => {
             status: 500
         })
     } else {
-        return response;
+        const data = await response.json();
+        return data.event;
     }
+}
+
+const loadEvents = async () => {
+    const response = await fetch('http://localhost:8080/events');
+
+    if (response.ok === false) {
+        return json(
+            {message: 'Could not fetch events'},
+            {
+                status: 500
+            }
+        );
+    } else {
+        const data = await response.json()
+        return data.events;
+    }
+}
+
+export const loader = async ({request, params}) => {
+    const { eventId: id } = params;
+
+    return defer({
+        event: await loadEvent(id),
+        events: loadEvents()
+    })
+
 }
 
 export const action = async ({request, params}) => {
