188f25cb7f646e0fcba1baa3fe63f6a8dd796b2c
Ivan Bieliaiev <belyaeve2@icloud.com>
2023-05-15 17:33:31 +0200

    Route actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import EventLayout from "./layouts/EventLayout";
 
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as addNewEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetail";
 import ErrorPage from "./pages/Error";
@@ -40,13 +40,14 @@ const router = createBrowserRouter([
                             },
                             {
                                 path: 'edit',
-                                element: <EditEventPage />
+                                element: <EditEventPage />,
                             },
                         ]
                     },
                     {
                         path: 'new',
-                        element: <NewEventPage />
+                        element: <NewEventPage />,
+                        action: addNewEventAction
                     }
                 ]
             },
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,7 +1,41 @@
+import { json, redirect } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 const NewEventPage = (props) => {
     return (
-        <h1>New Event Page</h1>
+        <EventForm />
     )
 }
 
 export default NewEventPage;
+
+export const action = async ({request, params}) => {
+    const data = await request.formData();
+
+    const eventData = {
+        title: data.get('title'),
+        image: data.get('image'),
+        date: data.get('date'),
+        description: data.get('description'),
+    };
+
+    const response = await fetch('http://localhost:8080/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventData)
+    })
+
+    if (response.ok === false) {
+        throw json({
+            message: 'Error on add new Event!'
+        },
+            {
+                status: 500
+            });
+    }
+
+    return redirect('/events');
+}
